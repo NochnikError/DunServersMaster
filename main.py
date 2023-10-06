d046c8f08eb8bc1d99974bb6906e62f5970a0309
Your Name <nochnikgang@gmail.com>
2023-10-07 02:35:04 +0300

    added response to buttons

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ bot = telebot.TeleBot(config['API_KEY'])
 print(config['teg'])
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    make_menu(message)
-
-
 def send_welcome(message):
     bot.reply_to(message, "As`Salam Aleikum, brat!")
 
@@ -22,16 +18,24 @@ def make_menu(message):
     markup.add(btn_info)
     bot.send_message(message.from_user.id, "commands:\nmenu", reply_markup=markup)
 
+
+def start(message):
+    make_menu(message)
 # @bot.message_handler(commands=['start'])
 # def button_message(message):
 #     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 #     btn_menu = types.KeyboardButton("/menu")
 #     markup.add(btn_menu)
 #     bot.send_message(message.chat.id, '/Menu', reply_markup=markup)
+@bot.message_handler(content_types='text')
+def button_back(message):
+    if message.text == 'back':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_back = types.KeyboardButton("Back")
+        markup.add(btn_back)
 
-
-@bot.message_handler(content_types=['text'])
-def message_reply(message):
+@bot.message_handler(content_types='text')
+def button_message(message):
     if message.text == "/menu":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn_cpu = types.KeyboardButton("CPU")
@@ -47,7 +51,25 @@ def message_reply(message):
         markup.add(btn_ram)
         markup.add(btn_usr)
         bot.send_message(message.chat.id, 'Press the required button', reply_markup=markup)
-    # elif message.text=="":
-    #     bot.send_message(message.chat.id,'')
+    elif message.text == "CPU":
+        button_back()
+        bot.send_message(message.chat.id, "CPU_INFO")
+    elif message.text == "DISK":
+        button_back()
+        bot.send_message(message.chat.id, text="DISK_INFO")
+    elif message.text == "NET":
+        button_back()
+        bot.send_message(message.chat.id, text="NET_INFO")
+    elif message.text == "PID":
+        button_back()
+        bot.send_message(message.chat.id, text="PID_INFO")
+    elif message.text == "RAM":
+        button_back()
+        bot.send_message(message.chat.id, text="RAM_INFO")
+    elif message.text == "USERS":
+        button_back()
+        bot.send_message(message.chat.id, text="USERS_INFO")
+
+
 
 bot.infinity_polling()
