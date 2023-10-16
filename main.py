f02238af187f116c7ab4c1c35d2a4de1e3fe8323
Your Name <nochnikgang@gmail.com>
2023-10-17 02:45:50 +0300

    feat(main):added bd for /start

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+import os
+import hashlib
 import logging
+import psycopg2
+from psycopg2 import Error
 import dotenv
 import telebot
 from telebot import types
+from subprocess import check_output
 from src.func_util.cpu import CpuUsageInfo, RamUsageInfo, DiskUsageInfo, NetUsageInfo, PIDUsageInfo
-import smtplib as smtp
+user_id = 0
+try:
+    connection = psycopg2.connect(user="postgres", password="postgres", host="localhost", port="5432", database="postgres")
+    cursor = connection.cursor()
+    print("Информация о сервере PostgreSQL")
+    print(connection.get_dsn_parameters(), "\n")
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("Вы подключены к - ", record, "\n")
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+
 
 
 logging.basicConfig(level=logging.DEBUG, filename="test.log", filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
+
 logging.debug("A DEBUG Message")
 logging.info("An INFO")
 logging.warning("A WARNING")
 logging.error("An ERROR")
 logging.critical("A message of CRITICAL severity")
 
-version = 0.05
+version = 0.06
 config = dotenv.dotenv_values()
 bot = telebot.TeleBot(config['API_KEY'])
 print(config['teg'])
@@ -22,7 +40,17 @@ print(config['teg'])
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome to the club, goi!\nSend (/help) me for details ")
+    user_id = message.chat.id
+    body = (message.chat.id, message.from_user.first_name, message.from_user.last_name, message.from_user.username)
+    print(type(body))
+    if user_id == cursor.execute("SELECT id_user FROM login_bot"):
+        print("Waiting login:", body)
+        bot.reply_to(message, "Welcome to the club. Again.\nSend (login), for authorization")
+    else:
+        cursor.execute("INSERT INTO login_bot (id_user, first_name,  last_name, username) VALUES (%s, %s, %s, %s)", body)
+        connection.commit()
+        print("New member:", body)
+        bot.reply_to(message, "Welcome to the club, goi!\nSend (/register) me for details ")
 
 
 @bot.message_handler(commands=['help'])
@@ -72,7 +100,31 @@ def check_credentials(password_message, login_message):
         bot.delete_message(password_message.chat.id, password_message.id)
 
 
+# Здесь необходимо вытащить из всего вывода именно сообщения, конвертировать его в байты по utf-8 и после этого хэшировать данные.
 @bot.message_handler(commands=['register'])
+def get_login(message):
+    reg_login = bot.send_message(message.chat.id, "Create a username for login in this bot:")
+    bot.delete_message(message.chat.id, message)
+    bot.register_next_step_handler(get_login, treatment_login)
+
+
+def treatment_login(get_login):
+    salt = os.urandom(128)
+    get_login = get_login.text
+    reg_login = get_login.encode('utf-8')
+    login = hashlib.pbkdf2_hmac('sha256', reg_login, salt, 256000)
+    storage = salt + login
+    bot.register_next_step_handler(get_login, get_password)
+
+
+def get_password(message):
+    reg_pass = bot.send_message(message.chat.id, "Cool, create a password:")
+    salt_for_pass = os.urandom(256)
+    password = hashlib.pbkdf2_hmac('sha256', reg_pass.text, salt_for_pass, 512000)
+    storage = salt_for_pass + password
+    bot.delete_message(message.chat.id, reg_pass.chat.id)
+
+
 @bot.message_handler(commands=['menu'])
 def menu_message(message):
     trank = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
@@ -98,6 +150,7 @@ def button_message(message):
         btn_pid = types.KeyboardButton("/pid")
         btn_swap = types.KeyboardButton("/swap")
         btn_usr = types.KeyboardButton("/users")
+        btn_cmd = types.KeyboardButton("/command")
         markup.add(btn_cpu)
         markup.add(btn_disk)
         markup.add(btn_net)
@@ -105,6 +158,7 @@ def button_message(message):
         markup.add(btn_ram)
         markup.add(btn_swap)
         markup.add(btn_usr)
+        markup.add(btn_cmd)
         bot.send_message(message.chat.id, 'Press the required button', reply_markup=markup)
     elif message.text == "/cpu":
         cpu_info = CpuUsageInfo()
@@ -126,6 +180,18 @@ def button_message(message):
         bot.send_message(message.chat.id, text=pid.print_top_pid())
     elif message.text == "/users":
         bot.send_message(message.chat.id, text="to soon")
+    elif message.text == "/command":
+        bot.send_message(message.chat.id, text="Write a command to execute:")
+        bot.register_next_step_handler(message, command)
+
+
+def command(message):
+    if user_id == (354939115, 415172037):
+        cmd = message.text
+        try:
+            bot.send_message(message.chat.id, check_output(cmd, shell=True))
+        except:
+            bot.send_message(message.chat.id, "Invalid input")
 
 
 bot.infinity_polling()
--- src/tests.py
+++ src/tests.py
@@ -1,68 +1,37 @@
-import psutil
-from psutil._common import bytes2human
-from pprint import pprint as pp
-from rich.console import Console
-from rich.table import Table, Column
-import datetime
+import psycopg2
+from psycopg2 import Error
+# Инициализация бд
 
-# pp([
-#     (p.pid, p.info)
-#     for p in psutil.process_iter(['name', 'status', 'cpu_times'])
-#     if p.info['status'] == psutil.STATUS_RUNNING
-#     and p.info(float['cpu_times']) >= 25.0
-#    ])
+try:
+    connection = psycopg2.connect(user="postgres", password="postgres", host="localhost", port="5432", database="postgres")
 
+    cursor = connection.cursor()
+    # create_table_query = '''CREATE TABLE login_bot
+    #                       (ID_USER INT PRIMARY KEY     ,
+    #                       FIRST_NAME           TEXT    ,
+    #                       LAST_NAME            TEXT    ,
+    #                       USERNAME             TEXT    ,
+    #                       ACCESS_LEVEL         TEXT    ,
+    #                       LOGIN_USER           TEXT    ,
+    #                       PASSWORD_USER        TEXT
+    #                       );'''
+    # cursor.execute(create_table_query)
+    # connection.commit()
+    # print("BD created.")
+    fsj = (2178717, "test", "tset", "hui")
+    cursor.execute("INSERT INTO login_bot (id_user, first_name,  last_name, username) VALUES (%s, %s, %s, %s)", fsj)
+    connection.commit()
+    print("Данные добавлены")
+    print("Информация о сервере PostgreSQL")
+    print(connection.get_dsn_parameters(), "\n")
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("Вы подключены к - ", record, "\n")
 
-class DiskUsageInfo:
-    _console = Console(width=24)
-
-    def print_stats(self):
-        print('\n------------\nCтатистикa смонтированных разделов диска\n------------\n')
-        print(self.get_stats_message())
-
-    def display_time(self, seconds, granularity=2):
-        result = []
-        intervals = (
-            ('weeks', 604800),  # 60 * 60 * 24 * 7
-            ('days', 86400),  # 60 * 60 * 24
-            ('hours', 3600),  # 60 * 60
-            ('minutes', 60),
-            ('seconds', 1),
-        )
-        for name, count in intervals:
-            value = seconds // count
-            if value:
-                self.seconds -= value * count
-                if value == 1:
-                    name = name.rstrip('s')
-                result.append("{} {}".format(value, name))
-        return ', '.join(result[:granularity])
-
-    def get_stats_message(self):
-        disks_stats = []
-        for part in psutil.users():
-            stats = {
-                'name': part.name,
-                'terminal': part.terminal,
-                'host': part.host,
-                'started': self.display_time(int(part.started)),
-                'pid': part.pid
-            }
-            grid = Table(show_header=False, expand=True, width=23, min_width=23)
-            grid.add_row('Name: ', stats['name'])
-            grid.add_row('Terminal: ', stats['terminal'])
-            grid.add_row('Host: ', stats['host'])
-            grid.add_row('Started: ', stats['started'])
-            grid.add_row('Pid: ', stats["pid"])
-
-            with self._console.capture() as capture:
-                self._console.print(grid)
-
-            disks_stats.append(capture.get())
-        text = '---------\n'.join(disks_stats)
-        return '```' + '\n' + text + '\n' + '```'
-#
-#
-# if __name__ == '__main__':
-#     mount = DiskUsageInfo()
-#     mount.print_stats()
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
